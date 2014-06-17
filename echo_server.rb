700e735a340aaa021f1234d476afa78c8e8e21d7
Stephens Xu <stephensxub@gmail.com>
2014-06-16 20:13:07 -0700

    completed echo_server.rb

--- echo_server.rb
+++ echo_server.rb
@@ -8,6 +8,9 @@ server = TCPServer.new(2000)
 loop do
   puts "Waiting for new clients on port #{server.addr[1]}..."
   client = server.accept
+  echo_message = client.gets
+  client.puts(echo_message)
+  client.close
 
   # Use client.gets to read a line of input from the client
   # Use client.puts to send a line of input to the client
