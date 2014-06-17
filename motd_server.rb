71935ac9a129e691219ab84732fcf33ab1cbe3d5
Stephens Xu <stephensxub@gmail.com>
2014-06-16 20:28:14 -0700

    completed motd_server.rb

--- motd_server.rb
+++ motd_server.rb
@@ -7,7 +7,12 @@ server = TCPServer.new(2000)
 
 loop do
   # 1. Wait for a client to connect.
+  puts "Waiting for new clients on port #{server.addr[1]}..."
   # 2. Read the contents of motd.txt using File.read
+  client = server.accept
+  message = File.read("motd.txt")
   # 3. Send the contents of motd.txt to the client
+  client.puts(message)
   # 4. Close the client's connection
+  client.close
 end
